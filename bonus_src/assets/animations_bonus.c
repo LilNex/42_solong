53f8e84777e0b2e5eceb3069f778a8fd8ab0c057
LilNex <nextorchq@gmail.com>
2023-06-21 18:58:59 +0100

    Fix heap buffer overflow

--- bonus_src/assets/animations_bonus.c
+++ bonus_src/assets/animations_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ichaiq <ichaiq@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/19 16:09:33 by ichaiq            #+#    #+#             */
-/*   Updated: 2023/06/21 17:18:46 by ichaiq           ###   ########.fr       */
+/*   Updated: 2023/06/21 18:58:15 by ichaiq           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ void	load_player_animation(t_solong *utils)
 	a->img[5] = load_xpm_file(utils, "./images/u6.xpm");
 	a->img[6] = load_xpm_file(utils, "./images/u7.xpm");
 	a->img[7] = load_xpm_file(utils, "./images/u8.xpm");
-	a->len = 3;
+	a->len = 8;
 	a->last = 0;
 	utils->a_data->player = a;
 }
@@ -53,7 +53,7 @@ void	load_collectible_asset(t_solong *utils)
 	t_animation	*asset;
 
 	asset = ft_calloc(1, sizeof(t_animation));
-	asset->img = ft_calloc(4, sizeof(void *));
+	asset->img = ft_calloc(5, sizeof(void *));
 	asset->img[0] = load_xpm_file(utils,
 			"./images/c1.xpm");
 	asset->img[1] = load_xpm_file(utils,
